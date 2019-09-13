da17fcfa2e3804ae54d5099f640ecab661214aa9
Juakata <ha.uzquiano@hotmail.com>
2019-09-13 15:45:47 -0600

    other methods

--- rest_client.rb
+++ rest_client.rb
@@ -12,11 +12,16 @@ def Search
     puts "Enter search query:"
     input = gets.chomp
 
-  
-    @response = RestClient.get url,{params: {'q' => input}} 
+
+    @response = RestClient.get url,{params: {'q' => input}}
 
    puts "Code: #{@response.code}"
 
    puts "Cookies: #{@response.cookies}"
 
+   puts "Headers: #{@response.headers}"
+
+   puts "Body: #{@response.body}"
+
+   puts "URL: #{@response.url}"
 end
